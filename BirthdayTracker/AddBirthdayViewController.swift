85ceb64c1c6d26ee6d20e48c9e73ec0fe9ff14f2
afoncom <afon.com12@gmail.com>
2025-09-03 20:40:23 +0300

    Initial commit: BirthdayTracker iOS app

---
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by afon.com on 18.05.2025.
+//
+
+import UIKit
+import CoreData
+import UserNotifications
+
+
+class AddBirthdayViewController: UIViewController {
+    
+    @IBOutlet  var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    @IBOutlet  var birthdatePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        birthdatePicker.maximumDate = Date()
+    }
+    
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        print("Меня зовут \(firstName) \(lastName).")
+        
+        let birthdate = birthdatePicker.date
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newBirthday = Birthday(context: context)
+        newBirthday.firstName = firstName
+        newBirthday.lastName = lastName
+        newBirthday.birthdate = birthdate as Date?
+        newBirthday.birthdayId = UUID().uuidString
+        
+        if let uniqueId = newBirthday.birthdayId {
+            print("birthdayId: \(uniqueId)")
+        }
+        
+        do {
+            try context.save()
+            let message = "Сегодня \(firstName) \(lastName) празднует день рождения!"
+            let content = UNMutableNotificationContent()
+            content.body = message
+            content.sound = UNNotificationSound.default
+            var dateComponents = Calendar.current.dateComponents([.month, .day], from: birthdate)
+            dateComponents.hour = 8
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            if let identifier = newBirthday.birthdayId {
+                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                let center = UNUserNotificationCenter.current()
+                center.add(request, withCompletionHandler: nil)
+            }
+        } catch let error {
+            print("Не удалось сохранить из-за ошибки \(error).")
+        }
+        
+        dismiss(animated: true, completion: nil)
+        print("Создана запись о дне рождения!")
+        
+    }
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+
---
+++ BirthdayTracker/BirthdaysTableViewController TableViewController.swift
@@ -0,0 +1,121 @@
+//
+//  BirthdaysTableViewController TableViewController.swift
+//  BirthdayTracker
+//
+//  Created by afon.com on 25.05.2025.
+//
+
+import UIKit
+import CoreData
+import UserNotifications
+
+class BirthdaysTableViewController_TableViewController: UITableViewController {
+    
+    var birthdays = [Birthday]()
+    
+    let dateFormatter = DateFormatter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dateFormatter.dateStyle = .full
+        dateFormatter.timeStyle = .none
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = Birthday.fetchRequest() as NSFetchRequest <Birthday>
+        
+        fetchRequest.sortDescriptors = [
+            .init(key:"lastName", ascending: true),
+            .init(key:"firstName", ascending: true)
+        ]
+        
+        do {
+            birthdays = try context.fetch(fetchRequest)
+        } catch let error {
+            print("Не удалось загрузить данные из - за ошибки: \(error).")
+        }
+        tableView.reloadData()
+    }
+    
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return birthdays.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "birthdayCellIdentifier", for: indexPath)
+        let birthday = birthdays[indexPath.row]
+        
+        let firstName = birthday.firstName ?? ""
+        let lastName = birthday.lastName ?? ""
+        cell.textLabel?.text = firstName + " " + lastName
+        
+        if let date = birthday.birthdate as Date? {
+            cell.detailTextLabel?.text = dateFormatter.string(from: date)
+        } else {
+            cell.detailTextLabel?.text = " "
+        }
+        
+        return cell
+    }
+    
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     
+    
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+         if birthdays.count > indexPath.row {
+             let birthday = birthdays[indexPath.row]
+             
+//              Удаляем уведомление
+             if let identifier = birthday.birthdayId {
+                 let center = UNUserNotificationCenter.current()
+                 center.removePendingNotificationRequests(withIdentifiers: [identifier])
+             }
+             
+             let appDelegate = UIApplication.shared.delegate as! AppDelegate
+             let context = appDelegate.persistentContainer.viewContext
+             context.delete(birthday)
+             birthdays.remove(at: indexPath.row)
+             tableView.deleteRows(at: [indexPath], with: .fade)
+             do {
+                 try context.save()
+             } catch let error {
+                 print("Не удалось сохранить из-за ошибки \(error).")
+             }
+         }
+     }
+     
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+}
